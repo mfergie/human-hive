8725b316c4b662129d799f13ad7d939be116ab39
Martin Fergie <mfergie@gmail.com>
2016-05-22 11:58:16 +0100

    Test scripts + initial hive work

---
+++ hive.py
@@ -0,0 +1,10 @@
+"""
+The Human Hive
+"""
+SAMPLE_RATE=44100
+
+def buzz(frequency, time):
+    """
+    Returns a buffer of a saw-tooth buzzing sound.
+    """
+    t = np.linspace(0, 1, SAMPLE_RATE)x
