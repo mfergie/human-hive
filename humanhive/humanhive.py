f66c7ef64482c14f9a3d5c509c7424bc40c26087
Martin Fergie <martin@Martins-iMac.local>
2016-06-25 18:03:53 +0100

    More work on getting basic structure done

--- humanhive/humanhive.py
+++ humanhive/humanhive.py
@@ -8,8 +8,35 @@ class HumanHive:
     HumanHive provides the overall access and control to the human hive system.
     """
 
-    def __init__(self):
-        assert False, "Unfinished"
+    def __init__(self,
+                 swarm_samples_dir,
+                 recorded_samples_dir=None,
+                 n_channels=2,
+                 sample_rate=44100,
+                 sample_width=2):
+
+        self.n_channels = n_channels
+        self.sample_rate = sample_rate
+        self.sample_width = sample_width
+
+        self.sample_bank = SampleBank(swarm_samples_dir, recorded_samples_dir)
+        self.recording = Recording(self.sample_bank)
+        self.playback = Playback(self.sample_bank)
+        self.audio_interface = AudioInterface(
+            self.playback,
+            self.recording,
+            self.n_channels,
+            self.sample_rate,
+            self.sample_width)
+
+
+    def start_stream(self):
+        self.audio_interface.start_stream()
+
+
+    def close_stream(self):
+        self.audio_interface.close_stream()
+
 
 
 class Playback:
@@ -17,8 +44,15 @@ class Playback:
     Manages the playback of sounds. Keeps active samples and controls volumes
     etc.
     """
+    
     def __init__(self, sample_bank):
         self.sample_bank = sample_bank
+
+
+    def retrieve_samples(self, frame_count):
+        # Do the audio stuff
+        assert False, "Unfinished"
+
 
 class Recording:
     """
@@ -29,7 +63,17 @@ class Recording:
     def __init__(self, sample_bank):
         self.sample_bank = sample_bank
 
-class SoundInterface:
+
+    def process_audio(in_data, frame_count):
+        """
+        Processes incoming audio data. Segments when a voice is detected and
+        records sample. This is then saved to the sample_bank.
+        """
+        pass
+
+
+
+class AudioInterface:
     """
     Manages the sound interface. This manages the main callback for the audio
     interface and delegates behaviour to the Playback and Recording modules.
@@ -58,11 +102,19 @@ class SoundInterface:
             output=True,
             stream_callback=self.audio_callback)
 
-        self.stream.start_stream()
-
 
     def audio_callback(self, in_data, frame_count, time_info, status):
-        assert False, "Unimplemented"
+        # Send recording data
+        self.recording.process_audio(in_data, frame_count)
+
+        # Get output audio
+        samples = self.playback.retrieve_samples(frame_count)
+
+        return (samples, pyaudio.paContinue)
+
+
+    def start_stream(self):
+        self.stream.start_stream()
 
 
     def close_stream(self):
