b456f406eaa8de2131c12fab01ceb34aa992ed87
Martin Fergie <mfergie@gmail.com>
2016-07-23 11:45:59 +0100

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='HumanHive',
+      version='1.0',
+      description='Sound software for the Human Hive',
+      author='Martin Fergie, Ellis Barnwell',
+      packages=['humanhive'],
+      scripts=['scripts/humanhive_run.py']
+     )
